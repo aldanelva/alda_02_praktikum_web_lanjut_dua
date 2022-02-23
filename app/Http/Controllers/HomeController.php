487e884b47f1d1a2e40ecc3981d4e15ac50b641d
aldanelva <aldamondel@gmail.com>
2022-02-23 14:15:02 +0700

    Home Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,17 +7,17 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        echo "Ini adalah halaman home";
+        echo "Selamat Datang";
     }
 
     public function about(){
-        echo "NIM   : 2041720158 ";
-        echo "Nama  : Alda Nelva Putri Sumita ";
-        echo "Kelas : TI 2D";
+        echo "NIM   : 2041720158 <br>";
+        echo "Nama  : Alda Nelva Putri Sumita<br>";
+        echo "Kelas : TI 2D<br>";
     }
 
     public function articles($id){
-        echo "Ini merupakan halaman dengan ID".$id;
+        echo "Halaman Artikel dengan ID ".$id;
     }
 
 }
